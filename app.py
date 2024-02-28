1efd4d0a36bd3db6074fbc959e4e1e5fe7abe1b9
Rafaelorr <154676586+Rafaelorr@users.noreply.github.com>
2024-02-28 14:13:26 +0100

    upload files

---
+++ app.py
@@ -0,0 +1,17 @@
+from flask import Flask,render_template,request
+import sqlite3
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template("home.html")
+
+@app.route("/bestel")
+def bestel():
+    if request.method == "POST":
+        pass
+    render_template()
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0",debug=True,port="50")
