b95fef57a6b10aa5aab2708d300372e13a99d724
Rafaelorr <rafael.onkelinx@muscardinus.be>
2024-03-03 14:10:08 +0100

    voeg create_acount toe aan app.py

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask,render_template,request,url_for
 import sqlite3
+from random import randint
 
 app:Flask = Flask(__name__)
 
@@ -18,7 +19,9 @@ def bestel():
     }
     con:sqlite3.Connection = sqlite3.connect('database.db')
     cur:sqlite3.Cursor = sqlite3.Cursor(con)
-
+    
+    
+    
     cur.executescript('INSERT INTO bestellingen (email,postcode,straatnaam,nummer) VALUES('+ bestelling["acountcode"] +','+bestelling["postcode"]+','+bestelling["straatnaam"]+','+bestelling["nummer"]+')')
     con.commit()
 
@@ -26,6 +29,24 @@ def bestel():
     con.close()
     render_template("succes.html")
   render_template("bestel.html")
+
+@app.route("/create_acount",methods=["GET","POST"])
+def create_acount():
+  if request.method == "POST":
+    con:sqlite3.Connection = sqlite3.connect('database.db')
+    cur:sqlite3.Cursor = sqlite3.Cursor(con)
+    while True:
+      acountcode = randint(1,9999999)
+      print(acountcode)
+      cur.execute("FROM acountcodes SELECT *")
+      acounts = cur.fetchall()
+      print(acounts)
+      if acountcode in acounts:
+        continue
+      else:
+        break
+
+    cur.executescript('INSERT INTO acountcodes (acountcodes,credits) VALUES('+acountcode+',50)')
 
 if __name__ == "__main__":
   app.run(host="0.0.0.0",debug=True)
