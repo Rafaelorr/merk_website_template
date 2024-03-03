b3776164832ebe2ae4ea03f1d788e03e53bd105d
Rafaelorr <rafael.onkelinx@muscardinus.be>
2024-03-03 13:44:13 +0100

    verwijder extra spaties uit app.py

--- app.py
+++ app.py
@@ -5,26 +5,26 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return render_template("home.html")
+  return render_template("home.html")
 
 @app.route("/bestel",methods=["GET","POST"])
 def bestel():
-    if request.method == "POST":
-        bestelling:dict = {
-            "email":request.form.get("email"),
-            "postcode":request.form.get("postcode"),
-            "straatnaam":request.form.get("straatnaam"),
-            "nummer":request.form.get("nummer")
-        }
-        con:sqlite3.Connection = sqlite3.connect('database.db')
-        cur:sqlite3.Cursor = sqlite3.Cursor(con)
+  if request.method == "POST":
+      bestelling:dict = {
+          "email":request.form.get("email"),
+          "postcode":request.form.get("postcode"),
+          "straatnaam":request.form.get("straatnaam"),
+          "nummer":request.form.get("nummer")
+      }
+      con:sqlite3.Connection = sqlite3.connect('database.db')
+      cur:sqlite3.Cursor = sqlite3.Cursor(con)
 
-        cur.executescript('INSERT INTO bestellingen (email,postcode,straatnaam,nummer) VALUES('+ bestelling["email"] +','+bestelling["postcode"]+','+bestelling["straatnaam"]+','+bestelling["nummer"]+')')
-        con.commit()
+      cur.executescript('INSERT INTO bestellingen (email,postcode,straatnaam,nummer) VALUES('+ bestelling["email"] +','+bestelling["postcode"]+','+bestelling["straatnaam"]+','+bestelling["nummer"]+')')
+      con.commit()
 
-        cur.close()
-        con.close()
-    render_template("bestel.html")
+      cur.close()
+      con.close()
+  render_template("bestel.html")
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0",debug=True)
+  app.run(host="0.0.0.0",debug=True)
