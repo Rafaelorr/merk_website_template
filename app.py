120950aabf7e276088de945e9f18c8d2bf9b262e
Rafaelorr <rafael.onkelinx@muscardinus.be>
2024-03-03 14:27:40 +0100

    maak acount_create route in app.py

--- app.py
+++ app.py
@@ -21,7 +21,6 @@ def bestel():
     cur:sqlite3.Cursor = sqlite3.Cursor(con)
     
     
-    
     cur.executescript('INSERT INTO bestellingen (email,postcode,straatnaam,nummer) VALUES('+ bestelling["acountcode"] +','+bestelling["postcode"]+','+bestelling["straatnaam"]+','+bestelling["nummer"]+')')
     con.commit()
 
@@ -36,17 +35,23 @@ def create_acount():
     con:sqlite3.Connection = sqlite3.connect('database.db')
     cur:sqlite3.Cursor = sqlite3.Cursor(con)
     while True:
-      acountcode = randint(1,9999999)
-      print(acountcode)
-      cur.execute("FROM acountcodes SELECT *")
-      acounts = cur.fetchall()
-      print(acounts)
-      if acountcode in acounts:
-        continue
-      else:
+      for _ in range(10000000):
+        acountcode = randint(1,9999999)
+        print(acountcode)
+        cur.execute("FROM acountcodes SELECT *")
+        acounts = cur.fetchall()
+        print(acounts)
+        if acountcode in acounts:
+          continue
+        else:
+          break
+      if acountcode not in acounts:
         break
-
+      else:
+        return "er zijn acountcodes meer over"
     cur.executescript('INSERT INTO acountcodes (acountcodes,credits) VALUES('+acountcode+',50)')
+    return render_template("create_acount_succes.html")
+  return render_template('create_acount.html')
 
 if __name__ == "__main__":
   app.run(host="0.0.0.0",debug=True)
