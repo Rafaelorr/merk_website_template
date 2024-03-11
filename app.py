1ef88c64d41c18aedd5a7253e8998322036f1a9e
Rafaelorr <rafael.onkelinx@muscardinus.be>
2024-03-11 19:32:32 +0100

    verwijder extra spaties in app.py

--- app.py
+++ app.py
@@ -3,18 +3,17 @@ from sqlite3 import Connection,connect,Cursor
 
 app:Flask = Flask(__name__)
 
-
 @app.route("/bestel",methods=["GET","POST"])
 def bestel():
   if request.method == "POST":
     con:Connection = connect('database.db')
     cur:Cursor = Cursor(con)
-    
-    
+
     cur.executescript(f"""INSERT INTO bestellingen (
-                      email,postcode,straatnaam,nummer) 
-                      VALUES('{request.form.get("acountcode")}','{request.form.get("postcode")}',
-                      '{request.form.get("straatnaam")}','{request.form.get("nummer")}')""")
+      email,postcode,straatnaam,nummer) 
+      VALUES('{request.form.get("acountcode")}','{request.form.get("postcode")}',
+      '{request.form.get("straatnaam")}','{request.form.get("nummer")}'
+    )""")
     con.commit()
 
     cur.close()
@@ -25,7 +24,6 @@ def bestel():
 @app.route("/")
 def home():
   return render_template("home.html",bestel_link=url_for("bestel"))
-
 
 if __name__ == "__main__":
   app.run(host="0.0.0.0",debug=True)
