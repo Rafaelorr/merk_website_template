fc8b42dbb5b71fb43973c1f3bfdb3072f06215fd
Rafaelorr <rafael.onkelinx@muscardinus.be>
2024-03-11 19:34:08 +0100

    verwijder extra spaties in database_setup.py

--- database_setup.py
+++ database_setup.py
@@ -4,12 +4,12 @@ con:Connection = connect('database.db')
 cur:Cursor = Cursor(con)
 
 cur.execute('''CREATE TABLE bestellingen (
-            id INTEGER PRIMARY KEY AUTOINCREMENT, 
-            email STRING, 
-            postcode INTEGER, 
-            straatnaam TEXT, 
-            nummer INTEGEER
-        )''')
+  id INTEGER PRIMARY KEY AUTOINCREMENT, 
+  email STRING, 
+  postcode INTEGER, 
+  straatnaam TEXT, 
+  nummer INTEGEER
+)''')
 
 cur.close()
 con.close()
