e02905f2c51e578f7ed3a7cd727aabc892d575a5
Rafael <rafael.onkelinx@muscardinus.be>
2024-03-20 09:07:42 +0100

    laatste build van de site

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app:Flask = Flask(__name__)
 @app.route("/bestel",methods=["GET","POST"])
 def bestel():
   if request.method == "POST":
-    con:sqlite3.Connection = sqlite3.connect('./database.db')
+    con:sqlite3.Connection = sqlite3.connect('database.db')
     cur:sqlite3.Cursor = sqlite3.Cursor(con)
 
     cur.executescript(f"""INSERT INTO bestellingen (
@@ -26,4 +26,4 @@ def home():
   return render_template("home.html",bestel_link=url_for("bestel"))
 
 if __name__ == "__main__":
-  app.run(host="0.0.0.0",debug=True)
+  app.run(host="0.0.0.0",debug=False)
